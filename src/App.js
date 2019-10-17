9c179ecb0940a184ed81d3b3fcf566e8aa4a72b2
calag4n <console.logan@gmail.com>
2019-10-18 01:57:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState}from 'react'
+import './App.css'
+import { format, getISOWeek, differenceInCalendarYears } from 'date-fns'
+
+
+
+
+const born = new Date(1985, 6, 29)
+
+const birthYear = differenceInCalendarYears(born, born)
+const birthWeek = getISOWeek(born, { weekStartsOn: 1 })
+const todayYear = differenceInCalendarYears(new Date(), born)
+const todayWeek = getISOWeek(new Date(), { weekStartsOn: 1 })
 
 function App() {
+
+  const [futurYear, setfuturYear] = useState(differenceInCalendarYears(new Date(), born))
+
+const [futurWeek, setfuturWeek] = useState(getISOWeek(new Date(), { weekStartsOn: 1 }))
+  console.log()
+
+
+  const handleChange = event => {
+    const date =new Date(event.target.value) 
+  
+    setfuturYear(differenceInCalendarYears(date, born))
+    setfuturWeek(getISOWeek(date , { weekStartsOn: 1 }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <h2>Naissance : </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>{format(born, 'dd/MM/yyyy')}</span> <br />
+          Année : <span>{birthYear}</span> . Semaine : <span>{birthWeek}</span>{' '}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>Aujourd'hui : </h2>
+        <p>
+          Année : <span> {todayYear} </span> . Semaine : <span>{todayWeek}</span>{' '}
+        </p>
+        <p>
+          Le : <input type='date' onChange={e => handleChange(e)}/> Année : <span>{futurYear || ''}</span>.
+          Semaine : <span>{futurWeek || ''}</span>{' '}
+        </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
